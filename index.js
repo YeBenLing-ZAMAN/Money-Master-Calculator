ae1bb51e35b46d503ba684dceaa4d2d1087783d1
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-02-17 11:38:05 +0600

    error handling with boostraps modal but it's would taka duble click, so it will be modify ofter this assignment result got

--- index.js
+++ index.js
@@ -1,43 +1,62 @@
 
 // Number and empty input valedation section  
 function forStringValuedation(amount, message) {
-    if (isNaN(amount) || (amount == '')) {
-        alert(`${message} input is not a number \n please Enter a Number.`);
+    if (isNaN(amount) || (amount == '') || (parseFloat(amount) < 0)) {
+        // alert(`${message} input is not a number \n please Enter a Number.`);
+        const errorHeading = document.createElement('li');
+        const error = document.getElementById('error-store');
+        errorHeading.innerHTML = `<li>${message} input is not a number \n please Enter a Number.</li>`;
+        error.appendChild(errorHeading);
+        return false
     } else {
-        return amount;
+        return true;
     }
 }
 
 // get input section 
 function getInput(idField) {
     const amount = document.getElementById(idField).value;
-    forStringValuedation(amount, idField);
-    return amount;
+    const inputValue = forStringValuedation(amount, idField);
+    if (true) {
+        return amount;
+    } else {
+        return '';
+    }
 }
 
 document.getElementById('balance-calculate').addEventListener('click', function (event) {
+    // refresh error 
+    const error = document.getElementById('error-store');
+    error.innerHTML = '';
     // input income and expensess with valuedation 
     const income = getInput('income');
     const food = getInput('food');
     const rent = getInput('rent');
     const cloths = getInput('cloths');
 
+    //error show
+    const erroeShow = document.getElementById('exampleModal');
+    if(error.innerHTML != ''){
+        if (true) {
+            document.getElementById('balance-calculate').setAttribute("data-bs-target", "#exampleModal");
+            document.getElementById('balance-calculate').setAttribute("data-bs-dismiss", "modal");
+
+        }
+    }
+ 
     // expenses calcution
     const totalExpenses = parseFloat(food) + parseFloat(rent) + parseFloat(cloths);
     const expenses = document.getElementById('expenses');
     expenses.innerText = totalExpenses;
 
     if (totalExpenses > parseFloat(income)) {
-        alert("income is less then expenses");
+        // alert("income is less then expenses");
     } else {
         // balance calution 
         const totalBalance = parseFloat(income) - totalExpenses;
         const balance = document.getElementById('balance');
         balance.innerText = totalBalance;
     }
-
-
-
 });
 
 
@@ -46,7 +65,7 @@ document.getElementById('balance-calculate').addEventListener('click', function 
 document.getElementById('save').addEventListener('click', function (event) {
     const income = getInput('income');
     const totalBalance = document.getElementById('balance').innerText;
-    
+
     // saving section 
     const incomePercetage = document.getElementById('percentage').value;
     const saving = parseFloat(income) * parseFloat(incomePercetage) / 100;
